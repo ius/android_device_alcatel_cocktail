c839d12857677178ea3cdd5bdfea76ff1e407202
Joerie de Gram <j.de.gram@gmail.com>
2012-10-16 21:23:13 +0200

    Restore bdaddr from WiFi MAC

--- recovery/wifi_mac.sh
+++ recovery/wifi_mac.sh
@@ -6,9 +6,10 @@ fi
 
 if [ $1 == "backup" ]; then
     grep -sh "^macaddr" /custpack/JRD_custres/wlan/nvram.txt \
-         /system/etc/wifi/bcmdhd.cal | head -n1 > /sdcard/wifi_mac.txt
+         /system/etc/wifi/bcmdhd.cal /sdcard/wifi_mac.txt | head -n1 > /sdcard/wifi_mac.txt
 elif [ $1 == "restore" ]; then
     cat /sdcard/wifi_mac.txt >> /system/etc/wifi/bcmdhd.cal
+    cat /sdcard/wifi_mac.txt | cut -d= -f2 >> /system/etc/bluetooth/bdaddr.txt
 else
     return 1
 fi
