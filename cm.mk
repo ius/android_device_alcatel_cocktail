c72ded26ad264f729ebc302f02291252d89bb13f
Joerie de Gram <j.de.gram@gmail.com>
2012-10-19 17:59:43 +0200

    Update fingerprint to ICS

--- cm.mk
+++ cm.mk
@@ -7,10 +7,11 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Spoof stock gingerbread fingerprint for now
+# Spoof stock fingerprint for now
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=one_touch_995_ALEU \
-                                BUILD_FINGERPRINT=TCT/one_touch_995_ALEU/one_touch_995_gsm:2.3.6/GINGERBREAD/v60J-0:user/release-keys \
-                                PRIVATE_BUILD_DESC="one_touch_995_ALEU-user 2.3.6 GINGERBREAD v60J-0 release-keys"
+                                BUILD_FINGERPRINT=TCT/one_touch_995_ALFR/one_touch_995_gsm:4.0.4/IceCreamSandwich/531:user/release-keys \
+                                PRIVATE_BUILD_DESC="one_touch_995_ALFR-user 4.0.4 IceCreamSandwich 531 release-keys"
+
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
